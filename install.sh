66aa3427f1d7633ceef3d086f9d15537dfb9dbbf
Pedro Zaroni <zaronipedro@outlook.com>
2021-08-04 14:36:15 -0300

    update install script

--- install.sh
+++ install.sh
@@ -21,4 +21,11 @@ npx husky install
 npx husky add .husky/commit-msg 'npx --no-install commitlint --edit $1'
 
 # add PR title lint github action
+GH_ACTIONS_FOLDER=$CURRENT_DIR/.github/workflows/
+echo $GH_ACTIONS_FOLDER
+
+if [ ! -d "$GH_ACTIONS_FOLDER" ]; then
+  mkdir .github/workflows/
+fi
+
 cp pull_request_lint.yml $CURRENT_DIR/.github/workflows/
