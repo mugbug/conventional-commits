f9b1fb5a27651ac979236eb190cce0c7f154c2d4
Pedro Zaroni <zaronipedro@outlook.com>
2021-08-04 14:27:40 -0300

    use pwd to get current dir

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+CURRENT_DIR=`pwd`
+# update gitignore
+echo "node_modules/" >> .gitignore
+
 # install commitlint
 npm install --save-dev @commitlint/{cli,config-conventional}
 
 # create commitlint config files
-cp commitlint.config.js .
-cp parser-preset.js .
+cp commitlint.config.js $CURRENT_DIR
+cp parser-preset.js $CURRENT_DIR
 
 # install husky
 npm install husky --save-dev
@@ -17,4 +21,4 @@ npx husky install
 npx husky add .husky/commit-msg 'npx --no-install commitlint --edit $1'
 
 # add PR title lint github action
-cp pull_request_lint.yml .github/workflows/
+cp pull_request_lint.yml $CURRENT_DIR/.github/workflows/
